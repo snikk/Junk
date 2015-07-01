49f7ea7c3c78de968ab198d279e91093b7864b2b
Jeffrey Glenn <jeff@jiff.com>
2015-07-01 16:36:21 -0700

    Tested some networking code.

--- test/glfw/shader/glsl/test.frag
+++ test/glfw/shader/glsl/test.frag
@@ -3,6 +3,6 @@
 out vec4 colorOut;
 
 void main() {
-    colorOut = vec4(1.0, 0.0, 1.0, 1.0);
+    colorOut = vec4(0.0, 0.0, 1.0, 1.0);
 }
 
---
+++ test/net/google.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define MAXDATASIZE 1000000
+
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char** argv) {
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN]; 
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo("httpbin.org", "http", &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("Client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("Client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+    printf("Client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo);
+
+    char msg[1000];
+    //int len = sprintf(msg, "");
+    int len = sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", "/ip", "httpbin.org");
+    printf("%s\n------------------------------------\n", msg);
+
+    if ((numbytes = send(sockfd, msg, len, 0)) == 0) {
+        perror("send");
+        exit(1);
+    }
+
+
+    printf("Bytes sent = %d | Msg length = %d\n", numbytes, len);
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    printf("client: received '%s'\n", buf);
+
+    close(sockfd);
+
+    return 0;
+}
