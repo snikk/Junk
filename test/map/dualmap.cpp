ca3ab52b723bedf3cade5f5f37bfef2b4485d3ae
Jeffrey Glenn <jeff@jiff.com>
2015-09-27 14:50:47 -0700

    Updated map testing.

--- test/map/dualmap.cpp
+++ test/map/dualmap.cpp
@@ -1,74 +1,123 @@
+#include <time.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <map>
+#include <unordered_map>
+#include <string>
 
-class Container {
-public:
-    static Container& instance();
-    void setValue(unsigned int i, bool b);
-    void updateValue();
-    void print();
-private:
-    static Container _cont;
+void mapTest(int seed, int count, long num);
+void unorderedMapTest(int seed, int count, long num);
+void stringMapTest(int seed, int count, long num);
+void stringUnorderedMapTest(int seed, int count, long num);
 
-    std::map<unsigned int, bool> map1;
-    std::map<unsigned int, bool> map2;
-};
+void createMap(int count);
+void createUnorderedMap(int count);
+void createStringMap(int count);
+void createStringUnorderedMap(int count);
+
+std::string getKey(long i);
+
+std::map<int, std::string> m;
+std::unordered_map<int, std::string> uom;
+std::map<std::string, std::string> sm;
+std::unordered_map<std::string, std::string> suom;
 
 int main(int argc, char** argv) {
-    Container::instance().print();
+    int seed = time(NULL);
+    int count = 10;
+    long testNumber = 100000000;
+    createMap(count);
+    mapTest(seed, count, testNumber);
+    createUnorderedMap(count);
+    unorderedMapTest(seed, count, testNumber);
 
-    Container::instance().setValue(122, true);
-
-    usleep(1000000);
-    Container::instance().print();
-
-    Container::instance().setValue(444, false);
-
-    usleep(1000000);
-    Container::instance().print();
-    usleep(1000000);
-    Container::instance().print();
-
-    std::map<unsigned int, bool> map1;
-    std::map<unsigned int, bool> map2;
-
-    map1[123] = true;
-    map1[444] = false;
-
-    for (auto& it : map1) {
-        printf("map1[%d] = %s\n", it.first, it.second ? "true" : "false");
-    }
-
-    for (auto& it : map2) {
-        printf("map2[%d] = %s\n", it.first, it.second ? "true" : "false");
-    }
+    createStringMap(count);
+    stringMapTest(seed, count, testNumber);
+    createStringUnorderedMap(count);
+    stringUnorderedMapTest(seed, count, testNumber);
 
     return 0;
 }
 
-Container& Container::instance() {
-    static Container _cont;
-    return _cont;
-}
+void mapTest(int seed, int count, long num) {
+    printf("mapTest | seed = %d\n", seed);
+    srand(seed);
+    int len = 0;
 
-void Container::setValue(unsigned int i, bool b) {
-    map1[i] = b;
-}
-
-void Container::print() {
-    printf("Printing values!\n");
-
-    for (auto& it2 : map2) {
-        printf("map2[%d] = %s\n", it2.first, it2.second ? "true" : "false");
+    for (int i = 0; i < num; i++) {
+        int idx = rand() % count;
+        len += m[idx].size();
     }
 
-    printf("---------------------------------------!\n");
+    printf("len = %d | float = %f\n", len, (float) len / (float) num);
+}
 
-    for (auto& it : map1) {
-        printf("map1[%d] = %s\n", it.first, it.second ? "true" : "false");
-        map2[it.first] = it.second;
+void unorderedMapTest(int seed, int count, long num) {
+    printf("unorderedMapTest | seed = %d\n", seed);
+    srand(seed);
+    int len = 0;
+
+    for (int i = 0; i < num; i++) {
+        int idx = rand() % count;
+        len += uom[idx].size();
     }
 
-    printf("Finished printing!\n\n\n");
+    printf("len = %d | float = %f\n", len, (float) len / (float) num);
+}
+
+void stringMapTest(int seed, int count, long num) {
+    printf("StringMapTest | seed = %d\n", seed);
+    srand(seed);
+    int len = 0;
+
+    for (int i = 0; i < num; i++) {
+        int idx = rand() % count;
+        len += sm[getKey(idx)].size();
+    }
+
+    printf("len = %d | float = %f\n", len, (float) len / (float) num);
+}
+
+void stringUnorderedMapTest(int seed, int count, long num) {
+    printf("stringUnorderedMapTest | seed = %d\n", seed);
+    srand(seed);
+    int len = 0;
+
+    for (int i = 0; i < num; i++) {
+        int idx = rand() % count;
+        len += suom[getKey(idx)].size();
+    }
+
+    printf("len = %d | float = %f\n", len, (float) len / (float) num);
+}
+
+void createMap(int count) {
+    for (int i = 0; i < count; i++) {
+        m[i] = "Number = " + std::to_string(i);
+    }
+
+}
+
+void createUnorderedMap(int count) {
+    for (int i = 0; i < count; i++) {
+        uom[i] = "Number = " + std::to_string(i);
+    }
+}
+
+void createStringMap(int count) {
+    for (int i = 0; i < count; i++) {
+        sm[getKey(i)] = "Number = " + std::to_string(i);
+    }
+
+}
+
+void createStringUnorderedMap(int count) {
+    for (int i = 0; i < count; i++) {
+        suom[getKey(i)] = "Number = " + std::to_string(i);
+    }
+}
+
+std::string getKey(long i) {
+    return "This is the key! | num = " + std::to_string(i);
 }
 
